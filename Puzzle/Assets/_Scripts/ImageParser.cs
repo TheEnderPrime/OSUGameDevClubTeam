e45ef0a6719b0a5bee2f63e065a17e5cf1425292
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2019-01-18 14:03:29 -0800

    Init branch

---
+++ Puzzle/Assets/_Scripts/ImageParser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct ImageData {
+	Sprite s;
+	
+};
+
+public class ImageParser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Sprite s = new Sprite();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
