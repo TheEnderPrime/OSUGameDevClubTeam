c8a17abfaf753e221f7caae635bd8349e3c6bfc3
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2019-01-18 14:06:32 -0800

    InitSetup

--- Puzzle/Assets/_Scripts/ImageParser.cs
+++ Puzzle/Assets/_Scripts/ImageParser.cs
@@ -2,20 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script obtains pre-split image files and divides them into their individual components, assigning an ID to each item
+
 struct ImageData {
 	Sprite s;
-	
+	int id;
 };
 
 public class ImageParser : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Sprite s = new Sprite();
-	}
-	
-	// Update is called once per frame
-	void Update () {
 		
 	}
+
 }
