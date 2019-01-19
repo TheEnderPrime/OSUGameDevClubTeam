5cc6f02bf6add70f79bc584d4a39814c9acbb609
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2019-01-18 16:43:32 -0800

    Snap feature :hammer:
    Yeet :ok_hand: :joy: :100:
    
    Issue #8

---
+++ Puzzle/Assets/_Scripts/TileSnapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSnapper : MonoBehaviour {
+
+	public bool mouseLeftDown;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		GameObject tile = col.gameObject; //Get game object connected to collider
+
+		//Check if the object has the puzzle piece tag
+		if (tile.CompareTag ("PuzzlePiece") && !mouseLeftDown) {
+			tile.transform.position = GetComponent<Transform>().position;
+			//TODO: On snap, update game state with new tile position in array
+		}
+	}
+}
