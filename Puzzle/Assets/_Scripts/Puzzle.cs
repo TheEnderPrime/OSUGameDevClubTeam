ab65264ebd05dc5cc1f30f2e0c339d067bc4053b
David Baugh <dbaugh.oregon@gmai.com>
2019-01-18 17:02:26 -0800

    Added read from file with text input puzzle spliting
    We can now text input the name of a puzzle in the OriginalImages folder, it will read from it and split it up. The splitting is far from perfect and needs work.

---
+++ Puzzle/Assets/_Scripts/Puzzle.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+
+	private Object[] spriteList;
+	[SerializeField] string puzzleName; // name of the puzzle
+	[SerializeField] public Texture2D source; // origianl image
+	public GameObject spritesRoot; // what object the split images are going to be connected to
+
+	// Use this for initialization
+	void Start () {
+
+		//NOTE!
+		//GameObject spritesRoot = GameObject.Find(puzzleName); 
+		//currently set in inspector, could be set here manually or dynamically
+
+		//get name of puzzle from user, set puzzleName
+
+
+		//get image from file
+		source = Resources.Load<Texture2D>("Images/OriginalImages/" + puzzleName);
+
+		//split sprite, needs to be change size dynamically, eventually based on difficulty
+		SplitSprite();	 
+
+		//get array from image
+		//GetArrayFromImage(puzzleName);
+		//sprite = gameObject.GetComponent<Sprite> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public static Texture2D LoadPNG(string puzzlename) {
+
+		Texture2D tex = null;
+		byte[] fileData;
+		var filePath = "Images/OriginalImages/" + puzzlename;
+
+		if (File.Exists(filePath))     {
+			fileData = File.ReadAllBytes(filePath);
+			tex = new Texture2D(1024, 1024);
+			tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+		}
+		return tex;
+	}
+
+	public void SplitSprite()
+	{
+		for(int i = 0; i < 8; i++)
+		{
+			for(int j = 0; j < 8; j++)
+			{
+				Sprite newSprite = Sprite.Create(source, new Rect(i*128, j*128, 128, 128), new Vector2(0.5f, 0.5f));
+				GameObject n = new GameObject();
+				SpriteRenderer sr = n.AddComponent<SpriteRenderer>();
+				sr.sprite = newSprite;
+				n.transform.position = new Vector3(i*2, j*2 , 0);
+				n.transform.parent = spritesRoot.transform;
+			}
+		}
+	}
+
+	Object[] GetArrayFromImage (string puzzlename)
+	{
+		spriteList = Resources.LoadAll ("Images/" + puzzlename, typeof(Sprite));
+		//Debug.Log(spriteList.Length);
+		foreach (var t in spriteList) {
+			Debug.Log (t.name);
+		}
+
+		return spriteList;
+	}
+}
