a778a842348084840abaf5eeb4eb448fc918f37f
ZTMMZ <ZTMMZ6203@hotmail.com>
2019-01-23 15:49:44 -0800

    Update Title Scene & Popups in Gameplay Scene
    ##TitleScene: has a menu to start game or exit the game.
    ##DummyGameScene: is a test scene for pause menu and win popup.
    ##BaseUI.cs: contains bunch of button events and keyboard event for UI.
    ##MoveObject.cs: for adding a controllable object to test the pause function.

---
+++ Puzzle/Assets/_Scripts/BaseUI.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BaseUI : MonoBehaviour {
+    protected GameObject canvas_main;
+    protected bool is_pause=false;
+
+    // Use this for initialization
+    void Start () {
+        canvas_main = GameObject.Find("Canvas");
+    }
+	
+    // Update is called once per frame
+	void Update () {
+        OnPressEsc();
+        if (!is_pause)
+        {
+            // do movement
+        }
+    }
+
+    //Click Events
+    public void OnClickSceneTrans(string SceneName) {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void OnClickPopup(string CanvasName) {
+        GameObject popup = canvas_main.transform.Find(CanvasName).gameObject;
+        popup.SetActive(true);
+    }
+
+    public void OnClickClosePopup() {
+        GameObject popup = gameObject.transform.parent.gameObject;
+        popup.SetActive(false);
+    }
+
+    public void OnClickExit() {
+        Application.Quit();
+    }
+
+    public void OnClickPause() {
+        Object[] objects = FindObjectsOfType(typeof(GameObject));
+        foreach (GameObject go in objects) {
+            go.SendMessage("OnPauseGame", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    public void OnClickRestart() {
+        Object[] objects = FindObjectsOfType(typeof(GameObject));
+        foreach (GameObject go in objects)
+        {
+            go.SendMessage("OnRestartGame", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    public void OnPauseGame() {
+        is_pause = true;
+    }
+
+    public void OnRestartGame() {
+        is_pause = false;
+    }
+
+    //KeyBorad Events
+    public void OnPressEsc() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            GameObject popupPause = canvas_main.transform.Find("Canvas_Popup_Pause").gameObject;
+            if(popupPause.activeInHierarchy) {
+                popupPause.SetActive(false);
+                OnClickRestart();
+            } else {
+                popupPause.SetActive(true);
+                OnClickPause();
+            }
+        }
+    }
+
+}
---
+++ Puzzle/Assets/_Scripts/MoveObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+    private Transform _transform;
+    public float speed = 10;
+    protected bool is_pause = false;
+
+	// Use this for initialization
+	void Start () {
+        _transform = this.transform;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(!is_pause) { 
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            _transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
+        }
+    }
+
+    public void OnPauseGame() {
+        is_pause = true;
+    }
+
+    public void OnRestartGame() {
+        is_pause = false;
+    }
+}
