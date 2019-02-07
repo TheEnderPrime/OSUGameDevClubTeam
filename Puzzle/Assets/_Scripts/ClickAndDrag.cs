3f032630bf78a14d7b8e50bf332873093b060789
David Baugh <dbaugh.oregon@gmail.com>
2019-02-07 11:41:52 -0800

    Added basic click and drag script

---
+++ Puzzle/Assets/_Scripts/ClickAndDrag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ClickAndDrag : MonoBehaviour {
+
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, -10f));
+	}
+	
+	void OnMouseDrag ()
+	{
+		Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+		transform.position = curPosition;
+	}
+}
