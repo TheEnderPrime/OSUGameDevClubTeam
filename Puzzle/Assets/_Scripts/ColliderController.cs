ed3cf88887a4c29cd8a41d86c9965550e2759e2a
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2019-02-13 19:53:59 -0800

    Good stuff

---
+++ Puzzle/Assets/_Scripts/ColliderController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderController : MonoBehaviour
+{
+	public int ID;
+	public bool isCorrect;
+
+	void Start() {
+		ID = -5;
+		isCorrect = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//TODO: 
+	}
+}
+
--- Puzzle/Assets/_Scripts/GridGenerator.cs
+++ Puzzle/Assets/_Scripts/GridGenerator.cs
@@ -10,12 +10,14 @@ public class GridGenerator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 		for(int i = 0; i < cols; i++) {
-			for(int k = 0; k < rows; k++)
-			{
+			for(int k = 0; k < rows; k++) {
 				GameObject clone = Instantiate(tileCollider);
 				clone.transform.position = new Vector3 (i, k, 0.0f);
+
+				//Set the ID
+				clone.GetComponent<TileSnapper>().ID = i + k;
 			}
 		}
 	}
