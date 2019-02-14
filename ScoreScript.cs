61a3e9998b5528540d9af9421b3c48118cb5b863
Shannon Sandy <sandys@oregonstate.edu>
2019-02-13 19:06:22 -0800

    Rough Score Card Added
    Score decreases as the amount of time passed increases. So far text is just displayed as blocks :/

---
+++ ScoreScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour {
+
+	public GameObject score_card;
+	//Add UI element
+	private float score;
+	private string score_txt;
+	TextMesh textMesh;
+	Font arial;
+
+	// Use this for initialization
+	void Start () {
+		arial = (Font)Resources.GetBuiltinResource (typeof(Font), "Arial.ttf");
+
+		score_card = GameObject.FindGameObjectWithTag ("Score Card");
+		MeshFilter mesh = score_card.GetComponent<MeshFilter> ();
+		DestroyImmediate (mesh);
+		textMesh = score_card.AddComponent<TextMesh> () as TextMesh;
+		textMesh.font = arial;
+		textMesh.characterSize = 0.03F;
+		textMesh.fontSize = 355;
+	
+		score = 1000;
+		score_txt = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score -= Time.deltaTime;
+		score_txt = score.ToString();
+		textMesh.text = score_txt;
+	}
+}
