0c70ddebdf49de72871810d8ac2c92615bacc064
jonesjonathan <jonesjon@oregonstate.edu>
2019-02-13 18:21:34 -0800

    Init changes

---
+++ Puzzle/Assets/_Scripts/GridGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+
+	public GameObject tileCollider;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 8; i++) {
+			for(int k = 0; k < 8; k++)
+			{
+				GameObject clone = Instantiate(tileCollider);
+				clone.transform.position.Set(i, k, 0);
+			}
+		}
+	}
+}
