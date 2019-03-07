f8a80079377236fa2a1438ad28ee5660ae0dcc55
Shannon Sandy <sandys@oregonstate.edu>
2019-03-06 18:51:41 -0800

    Added Score Card
    To implement, add a Text object under the Canvas directory. Add the Score Script to any puzzle piece object and add the Text object to the score_txt variable within.

---
+++ Puzzle/Assets/_Scripts/ScoreScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+	//public GameObject score_card;
+	//Add UI element
+	private float score;
+	public Text score_txt;
+
+
+	// Use this for initialization
+	void Start () {
+		//score_card = GameObject.FindGameObjectWithTag ("Score Card");
+
+
+		score = 1000;
+		SetScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score -= Time.deltaTime;
+		SetScore ();
+	}
+
+	void SetScore(){
+		score_txt.text = "Score: " + score.ToString ();
+	}
+}
